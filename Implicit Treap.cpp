6965924a2898aef4af390f0df2a10dbbc88e3b17
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-02-04 19:31:30 -0300

    Update Implicit Treap.cpp

--- Implicit Treap.cpp
+++ Implicit Treap.cpp
@@ -1,18 +1,31 @@
+mt19937 rng ((int) chrono::steady_clock::now().time_since_epoch().count());
+
+typedef int Key;
 typedef struct item * pitem;
 
 struct item {
-    int prior, value, cnt;
-    bool rev;
-    pitem l, r;
+  item () {}
+  item (Key k) {
+    key = k;
+    prio = uniform_int_distribution<int>() (rng);
+    cnt = 1;
+    rev = 0;
+    l = r = NULL;
+  }
+  pitem l, r;
+  Key key;
+  int prio;
+  int cnt;
+  bool rev;
 };
 
 int cnt (pitem it) {
-    return it ? it->cnt : 0;
+  return it ? it->cnt : 0;
 }
 
 void upd_cnt (pitem it) {
-    if (it)
-        it->cnt = cnt(it->l) + cnt(it->r) + 1;
+  if (it)
+    it->cnt = cnt(it->l) + cnt(it->r) + 1;
 }
 
 void push (pitem it) {
@@ -25,34 +38,25 @@ void push (pitem it) {
 }
 
 void merge (pitem & t, pitem l, pitem r) {
-    push (l);
-    push (r);
-    if (!l || !r)
-        t = l ? l : r;
-    else if (l->prior > r->prior)
-        merge (l->r, l->r, r),  t = l;
-    else
-        merge (r->l, l, r->l),  t = r;
-    upd_cnt (t);
+  push (l);
+  push (r);
+  if (!l || !r)
+    t = l ? l : r;
+  else if (l->prio > r->prio)
+    merge (l->r, l->r, r),  t = l;
+  else
+    merge (r->l, l, r->l),  t = r;
+  upd_cnt (t);
 }
 
-void split (pitem t, pitem & l, pitem & r, int key, int add = 0) {
-    if (!t)
-        return void( l = r = 0 );
-    push (t);
-    int cur_key = add + cnt(t->l);
-    if (key <= cur_key)
-        split (t->l, l, t->l, key, add),  r = t;
-    else
-        split (t->r, t->r, r, key, add + 1 + cnt(t->l)),  l = t;
-    upd_cnt (t);
-}
-
-void reverse (pitem t, int l, int r) {
-    pitem t1, t2, t3;
-    split (t, t1, t2, l);
-    split (t2, t2, t3, r-l+1);
-    t2->rev ^= true;
-    merge (t, t1, t2);
-    merge (t, t, t3);
+void split (pitem t, pitem & l, pitem & r, Key key, int add = 0) {
+  if (!t)
+    return void( l = r = 0 );
+  push (t);
+  Key cur_key = add + cnt(t->l);
+  if (key <= cur_key)
+    split (t->l, l, t->l, key, add),  r = t;
+  else
+    split (t->r, t->r, r, key, add + 1 + cnt(t->l)),  l = t;
+  upd_cnt (t);
 }

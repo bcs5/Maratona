5c70b6cacdd0ec3b4a457fa4d3bd27185a09565e
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-02-06 14:33:39 -0300

    Update Sack.cpp

--- Sack.cpp
+++ Sack.cpp
@@ -1,43 +1,45 @@
+int sz[ms];
+int t, in[ms], out[ms], rin[ms];
 
-void solve(int v, int par, bool f){
-	int big = -1;
-	for(auto &u : adj[v]){
-		if(u != par && (big == -1 || Size[u] > Size[big])){
-			big = u;
-		}
-	}
-	for(auto &u : adj[v]){
-		if(u == par || u == big) continue;
+void dfs_sz(int v = 0, int p = -1){
+  in[v] = t++;
+  rin[in[v]] = v;
+  sz[v] = 1;
+  
+  for(int i = 0; i < adj[v].size(); i++){
+    int &u = adj[v][i];
+    if(u == p) continue;
+    dfs_sz(u, v);
+    sz[v] += sz[u];
+    if(adj[v][0] == p || sz[u] > sz[adj[v][0]]){
+      swap(u, adj[v][0]);
+    }
+  }
+  out[v] = t;
+}
+
+void solve (int v, int p, bool f) {
+	for (int &u : adj[v]) {
+    if (u == p || u == adj[v][0]) continue;
 		solve(u, v, 0);
 	}
-	if(~big) solve(big, v, 1);
-	
-	// add curr info
-	add(cnt[v[v]], -1);
-	cnt[v[a]]++;
-	add(cnt[v[v]], +1);
-	
-	//add smallest children
-	for(auto &u : adj[v]){
-		if(u == par|| u == big) continue;
-		for(int i = st[u]; i < en[u]; i++){
-			add(cnt[ett[i]], -1);
-			cnt[ett[i]]++;
-			add(cnt[ett[i]], +1);
+	if(adj[v].size() > 1) solve(adj[v][0], v, 1);
+  
+	// add v
+	// add smallest children
+  for(auto &u : adj[v]){
+		if(u == p|| u == adj[v][0]) continue;
+		for(int i = in[u]; i < out[u]; i++){
+			// add rin[i];
 		}
 	}
 	
 	// ans queries for v subtree
-	for(auto &q : Q[v]){
-		ans[q.first] = query(mx-1)-query(q.second-1);
-	}
 	
 	// clear
 	if(!f){
-		for(int i = st[v]; i < en[v]; i++){
-			add(cnt[ett[i]], -1);
-			cnt[ett[i]]--;
-			add(cnt[ett[i]], +1);
+		for(int i = in[v]; i < out[v]; i++){
+			// rem rin[i];
 		}
 	}
 }

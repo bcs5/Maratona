b88bbacfa9bf70c67aa676d8a6620add60ae10a9
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-02-07 23:18:17 -0300

    Create BiconnectedComponents.cpp

---
+++ BiconnectedComponents.cpp
@@ -0,0 +1,55 @@
+const int ms = 1e3; // Quantidade maxima de vertices
+const int me = 1e5; // Quantidade maxima de arestas
+
+int adj[ms], to[me], ant[me], z, n;
+int idx[ms], bc[me], art[ms], bridge[me], ind, nbc, child, st[me], top;
+
+void clear() { // Lembrar de chamar no main
+  memset(adj, -1, sizeof adj);
+  z = 0;
+}
+
+void add(int u, int v) {
+  to[z] = v;
+  ant[z] = adj[u];
+  adj[u] = z++;
+}
+
+void generateBc(int edge) {
+  while(st[--top] != edge) {
+    bc[st[top]] = nbc;
+  }
+  bc[edge] = nbc++;
+}
+
+int dfs(int v, int par = -1) {
+  int low = idx[v] = ind++;
+  for(int i = adj[v]; i > -1; i = ant[i]) {
+    if(idx[to[i]] == -1) {
+      if(par == -1) child++;
+      st[top++] = i;
+      int temp = dfs(to[i], v);
+      if(par == -1 && child > 1 || ~par && temp >= idx[v]) generateBc(i);
+      if(temp >= idx[v]) art[v] = true;
+      if(temp > idx[v]) bridge[i] = true;
+      low = min(low, temp);
+    } else if(to[i] != par && idx[to[i]] < low) {
+      low = idx[to[i]];
+      st[top++] = i;
+    }
+  }
+  return low;
+}
+
+void biconnected() {
+  ind = 0;
+  nbc = 0;
+  top = -1;
+  memset(idx, -1, sizeof idx);
+  memset(art, 0, sizeof art);
+  memset(bridge, 0, sizeof bridge);
+  for(int i = 0; i < n; i++) if(idx[i] == -1) {
+    child = 0;
+    dfs(i);
+  }
+}

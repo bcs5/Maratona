e111004d5600b6c2f34c7d6d6d027dfccb96fe98
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-02-08 16:13:44 -0300

    Update BiconnectedComponents.cpp

--- BiconnectedComponents.cpp
+++ BiconnectedComponents.cpp
@@ -1,20 +1,21 @@
-int num[ms], low[ms], timer, rch;
-//int art[ms], bridge[me];
+int num[ms], low[ms], timer;
+int art[ms], bridge[me], rch;
 int bc[ms], nbc;
-int st[ms], top;
+stack<int> st;
 bool f[me];
 
 void generateBc (int v) {
   ++nbc;
-  while (top) {
-    int u = st[--top];
+  while (!st.empty()) {
+    int u = st.top();
+    st.pop();
     bc[u] = nbc;
     if (v == u) break;
   }
 }
 
 void dfs (int v, int p) {
-  st[top++] = v;
+  st.push(v);
   low[v] = num[v] = ++timer;
   for (int i = adj[v]; i != -1; i = ant[i]) {
     if (f[i] || f[i^1]) continue;
@@ -22,13 +23,13 @@ void dfs (int v, int p) {
     int u = to[i];
     if (num[u] == -1) {
       dfs(u, v);
-      //if (low[u] > num[v]) bridge[i] = bridge[i^1] = 1;
-      //art[v] |= p != -1 && low[u] >= num[v];
-      //if (p == -1 && rch > 1) art[v] = 1;
-      //else rch ++;
+      if (low[u] > num[v]) bridge[i] = bridge[i^1] = 1;
+      art[v] |= p != -1 && low[u] >= num[v];
+      if (p == -1 && rch > 1) art[v] = 1;
+      else rch ++;
       low[v] = min(low[v], low[u]);
     } else {
-      low[v] = min(num[v], low[u]);
+      low[v] = min(low[v], num[u]);
     }
   }
   if (low[v] == num[v]) generateBc(v);

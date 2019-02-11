68b1623a8b3adaee56dd1dc0095a692b3dd9a4ce
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-02-11 01:08:51 -0300

    Create mat.cpp

---
+++ mat.cpp
@@ -0,0 +1,27 @@
+#define DIM 2
+
+template <typename T>
+struct mat{
+  T a[DIM][DIM];
+  
+	mat () {
+		memset(a, 0, sizeof(T) * DIM * DIM);
+	}
+	
+	void identity () {
+	  for (int i = 0; i < DIM; ++i)
+				for (int j = 0; j < DIM; ++j) a[i][j] = i == j;
+	}
+	
+	mat<T> operator * (const mat<T> &b) const {
+		mat<T> c;
+		for (int k = 0; k < DIM; k++) { 
+			for (int i = 0; i < DIM; i++) {
+				for (int j = 0; j < DIM; j++) {
+					c.a[i][j] = (c.a[i][j] + a[i][k] * b.a[k][j]);
+				}
+			}
+		}
+		return c;
+	}
+};

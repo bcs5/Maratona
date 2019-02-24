9f6a479a5eabeefacac4f66c5db8e131a8a1bb89
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-02-23 23:06:24 -0300

    Create Color Upadte.cpp

---
+++ Color Upadte.cpp
@@ -0,0 +1,34 @@
+set<range> ranges;
+
+void split (int x) {
+  auto it = ranges.lower_bound(x);
+  if (it == ranges.begin()) return;
+  it--;
+  if (it->r > x) {
+    auto cur = *it;
+    ranges.erase(it);
+    ranges.insert(range(cur.l, x, cur.v));
+    ranges.insert(range(x, cur.r, cur.v));
+  }
+}
+
+vector<range> update (int l, int r, int v) { // open interval
+  vector<range> ans;
+  if (l >= r) return ans;
+  split(l), split(r);
+  for (auto it = ranges.lower_bound(l); it != ranges.end() && it->l < r; it++) {
+    ans.push_back(*it);
+  }
+  ranges.erase(ranges.lower_bound(l), ranges.lower_bound(r));
+  ranges.insert(range(l, r, v));
+  return ans;
+}
+
+int query (int v) {
+  auto it = ranges.upper_bound(v);
+  if (it == ranges.begin()) {
+    return -1;
+  }
+  it--;
+  return it->r >= v ? it->v : -1;
+}

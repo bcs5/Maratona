3367955400eebe018a4ae8d1774308c6d3ad2923
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-03-07 16:31:09 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(foo) foo.begin(), foo.end()
+
+#define sc(a) scanf("%d", &a)
+#define sc2(a,b) scanf("%d%d", &a, &b)
+#define sc3(a,b,c) scanf("%d%d%d", &a, &b, &c)
+#define pri(x) printf("%d\n", x)
+
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define x first
+#define y second
+
+
+typedef long long ll;
+typedef pair<int, int> ii;
+
+const int inf = 0x3f3f3f3f;

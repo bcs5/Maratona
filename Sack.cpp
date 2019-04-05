38bd40f32d44e180fccabe5e2099cdb8c13adc28
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-04-05 16:12:34 -0300

    Update Sack.cpp

--- Sack.cpp
+++ Sack.cpp
@@ -8,11 +8,16 @@ void dfs_sz (int v = 0, int p = -1) {
   
   for (int i = 0; i < adj[v].size(); i++) {
     int &u = adj[v][i];
-    if (u == p) continue;
-    dfs_sz(u, v);
-    sz[v] += sz[u];
-    if (adj[v][0] == p || sz[u] > sz[adj[v][0]]) {
-      swap(u, adj[v][0]);
+    if (u == p) {
+      swap(u, adj.back());
+      adj.pop_back();
+      i--;
+    } else {
+      dfs_sz(u, v);
+      sz[v] += sz[u];
+      if (sz[u] > sz[adj[v][0]]) {
+        swap(u, adj[v][0]);
+      }
     }
   }
   out[v] = t;
@@ -20,15 +25,14 @@ void dfs_sz (int v = 0, int p = -1) {
 
 void solve (int v, int p, bool f) {
   for (int &u : adj[v]) {
-    if (u == p || u == adj[v][0]) continue;
+    if (u == adj[v][0]) continue;
     solve(u, v, 0);
   }
-  if (adj[v].size() > 1) solve(adj[v][0], v, 1);
-
+  if (!adj[v].empty()) solve(adj[v][0], v, 1);
   // add v
   // add smallest children
   for (auto &u : adj[v]) {
-    if (u == p|| u == adj[v][0]) continue;
+    if (u == adj[v][0]) continue;
     for (int i = in[u]; i < out[u]; i++) {
       // add rin[i];
     }

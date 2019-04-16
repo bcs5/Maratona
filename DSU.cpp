c49405116bdf8045d0f52f38246aaf9e20257f3a
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-04-16 16:23:45 -0300

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,6 +1,8 @@
 struct dsu {
   vector<int> hist, par, sz;
-  void init (int n) {
+  vector<ii> changes;
+  int n;
+  dsu (int n) : n(n) {
     hist.assign(n, 1e9);
     par.resize(n);
     iota(par.begin(), par.end(), 0);
@@ -15,15 +17,28 @@ struct dsu {
   void join (int a, int b, int t) {
     a = root(a, t);
     b = root(b, t);
-    if (a == b) return;
+    if (a == b) { changes.emplace_back(-1, -1); return; }
     if (sz[a] > sz[b]) swap(a, b);
     par[a] = b;
     sz[b] += sz[a];
     hist[a] = t;
+    changes.emplace_back(a, b);
+    n--;
   }
   
   bool same (int a, int b, int t) {
     return root(a, t) == root(b, t);
+  }
+  
+  void undo () {
+    int a, b;
+    tie(a, b) = changes.back();
+    changes.pop_back();
+    if (a == -1) return;
+    sz[b] -= sz[a];
+    par[a] = a;
+    hist[a] = 1e9;
+    n++;
   }
   
   int when (int a, int b) {

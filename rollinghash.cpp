86ea5ddabfd0b9a00128e4c523ba1d2186ed2abe
Bezaliel Silva <33490486+bcs5@users.noreply.github.com>
2019-04-23 18:40:15 -0300

    Update rollinghash.cpp

--- rollinghash.cpp
+++ rollinghash.cpp
@@ -1,57 +1,28 @@
-// building
+
 // 1073676287
-T prime = 0xdefaced
-T mod1,  mod2;
-struct hash_k {
-  T h1, h2;
-  hash_k operator + (hash_t rh) {
-    return hash_k((rh.h1 + h1) % mod1, (rh.h2 + h2) % mod2);
-  }
-  hash_k operator - (hash_t rh) {
-    return hash_k((-rh.h1 + h1 + mod1) % mod1, (-rh.h2 + h2 + mod2) % mod2);
-  }
-   hash_k operator * (hash_t rh) {
-    T a = (((rh.h1 * h1) % mod1)+mod1)%mod1;
-    T b = (((rh.h2 * h2) % mod2)+mod2)%mod2;
-    return hash_k(a, b);
-  }
-}  
-vector<hash_k> primepot;
-
-struct hash_t {
-  hash_k key;
-  int sz;
-  hash_t (char c) { hash1 = t}
-  hash_t operator + (hash_t lh, hash_t rh) {
-    c.key * primepot[rh.sz] + rh;
-    c.sz = lh.sz + rh.sz;
-    return c;
-  }
-  hash_t operator - (hash_t lh, hash_t rh) {
-    c.sz = lh.sz - rh.sz + 1;
-    c.key = lh - 1*rh*primepot[c.sz];
-    return c;
-  }
-}
-
+template <typename T>
 struct stringh {
-  vector<hash_t> hl, hr;
-  stringh (string s = "") {
-    hl.resize(s.size()+2);
-    hr.resize(s.size()+2);
-    for (int i = 1; i <= s.size(); i++) {
-      hl[i] = hl[i-1] + hash_t(s[i-1]);
-    }
-    for (int i = s.size(); i > 0; i--) {
-      hr[i] = hr[i+1] + hash_t(s[i-1]);
+  string s;
+  vector<T> h;
+  vector<T> h_base;
+  T mod;
+
+  stringh (string s, T mod) : s(s), mod(mod) {
+    h.resize(s.size()+1);
+    h_base.resize(s.size()+1);
+    hash_init();
+  }
+
+  void hash_init (T prime = 0xdefaced) {
+    h_base[0] = 1;
+    h[0] = 0;
+    for (int i = 1; i <= s.size(); i++){
+      h[i] = (h[i-1] * prime + s[i-1]) % mod;
+      h_base[i] = (h_base[i-1] * prime) % mod;
     }
   }
-  
-  hash_t lget (int l, int r) {
-    return hl[r+1] - hl[l];
-  }
-  
-  hash_t rget (int l, int r) {
-    return hr[l+1] - hr[r];
+
+  T get (int l, int r) {
+    return (h[r+1] - (h[l] * h_base[r-l+1]) % mod + mod) % mod;
   }
 };
